9282c0d27bb1b1fd649938d17205d1777f5aca44
alicia <alicia@ringover.com>
2019-09-13 12:03:16 +0200

    Exo 2 finished

--- Exo 2/index.js
+++ Exo 2/index.js
@@ -1,1 +1,23 @@
 'use strict';
+
+function getBar() {
+	var elBody = document.querySelector('body');
+	var elDiv = document.createElement('div');
+	elDiv.style.background = 'blue';
+	elDiv.style.height = '30px';
+	elBody.appendChild(elDiv);
+	var width = 1;
+  	var id = setInterval(progress, 10);
+	var elem = document.querySelector('div');
+	function progress() {
+		if (width >= 100) {
+			clearInterval(id);
+
+		} else {
+			width++;
+			elem.style.width = width + '%';
+		}
+	}	
+}
+getBar();
+
