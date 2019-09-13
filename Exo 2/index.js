d56e0ed8cd6835d758f57a8bf77e900b44eb9f15
alicia <alicia@ringover.com>
2019-09-13 15:00:40 +0200

    correcting and stuff

--- Exo 2/index.js
+++ Exo 2/index.js
@@ -7,7 +7,7 @@ function getBar() {
 	elDiv.style.height = '30px';
 	elBody.appendChild(elDiv);
 	var width = 1;
-  	var id = setInterval(progress, 10);
+  	var id = setInterval(progress, 100);
 	var elem = document.querySelector('div');
 	function progress() {
 		if (width >= 100) {
@@ -17,7 +17,69 @@ function getBar() {
 			width++;
 			elem.style.width = width + '%';
 		}
+
 	}	
 }
 getBar();
 
+
+
+////////////// CORRECT WAY //////////////
+
+
+/* 
+'use strict';
+​
+/**
+ * DrawBarre
+ * @construtor
+ * @param {Object} el
+ */
+/*
+var DrawBarre = function DrawBarre(el) {
+	this.el = document.querySelector(el);
+	this.elSegment = document.createElement('div');
+  }
+  ​
+  /**
+   * render
+   */
+  /*
+  DrawBarre.prototype.render = function () {
+	this.el.appendChild(this.renderBarre(function() {
+	  this.loading();
+	}.bind(this)));
+  }
+  ​
+  DrawBarre.prototype.loading = function() {
+	setInterval(function() {
+	  this.elSegment.style.width = this.elSegment.dataset.progress + '%'; 
+	  this.elSegment.dataset.progress = parseInt(this.elSegment.dataset.progress) + 1;
+	}.bind(this), 500);
+  }
+  ​
+  DrawBarre.prototype.renderBarre = function (callback) {
+	var barre = document.createElement('div');
+	
+	barre.style.background = 'red';
+	barre.style.height = '30px';
+	barre.style.width = '100%';
+  ​
+	this.elSegment.style.background = 'green';
+	this.elSegment.style.height = '30px';
+	this.elSegment.style.width = '0%';
+	this.elSegment.setAttribute('data-progress', '0');
+  ​
+  ​
+  ​
+	barre.appendChild(this.elSegment);
+  ​
+	callback();
+  ​
+	return barre;
+  }
+  ​
+  var drawBarre = new DrawBarre('.toto');
+  ​
+  drawBarre.render();
+  */
