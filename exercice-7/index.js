50435732ec5ab90e6d7f0146c19e08fcb36f0475
Alicia <alicia@ringover.com>
2019-10-30 16:55:48 +0100

    finished

--- exercice-7/index.js
+++ exercice-7/index.js
@@ -2,16 +2,19 @@
 
 function computeNotes() {
 
+	//define rates
 	var tab = [12, 13, 14, 11, 20, 19, 15];
 	var i = 0;
 	var tot = 0.0;
 
+	//walking in the array and sum all values
 	while (i < tab.length) {
 		tot += tab[i];
 		i++;
-    }
+	}
+	//divide sum by number of rate
 	return (tot / tab.length);
 }
-
+//launch
 var result = computeNotes();
 console.log(result);
